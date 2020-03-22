a24f41c164db0f2fc5e600a63e758bb778ea1314
valyse <vasileva.liuba@gmail.com>
2020-03-22 18:17:06 +0300

    add project

---
+++ TestAPI/ApiTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+
+namespace TestAPI
+{
+    [TestClass]
+    public class ApiTest
+    {
+        private City hometown;
+
+        [TestMethod]
+        public void CheckSPbFriends()
+        {
+
+            var api = new VkNet.VkApi();
+            api.Authorize(new ApiAuthParams
+            {
+                AccessToken = "2583689c8e583ccb25b4b825b5795f904cedae4e8b96e585e0be83c1abf27a51551ad528674842e18d339"
+            });
+
+            VkNet.Utils.VkCollection<User> friends = api.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+            {
+                UserId = 4599649,
+                Fields = ProfileFields.City,
+            });
+
+            hometown = api.Account.GetProfileInfo().City;
+            int count = 0;
+            
+            foreach (var f in friends)
+            {
+                if (f.City == null)
+                    continue;
+
+                if (f.City.Title == hometown.Title)
+                {
+                    count++;
+                }
+            }
+
+            Assert.AreEqual(count, 48);
+        }
+    }
+}
---
+++ TestAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestAPI")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c5ab77bf-7d78-4fa3-a0f3-cb49222969d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
